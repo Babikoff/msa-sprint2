19fe60602310406279047374a57b059657ed822f
Ivan Babikov <i.babikov@itransition.com>
2026-08-13 20:53:10 +0500

    Добавлен микросервис booking-service

---
+++ tasks/task2/booking-service/src/main/java/com/hotelio/monolith/service/AppUserService.java
@@ -0,0 +1,55 @@
+package com.hotelio.monolith.service;
+
+// import com.hotelio.monolith.entity.AppUser;
+// import com.hotelio.monolith.repository.AppUserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class AppUserService {
+
+    // private final AppUserRepository repository;
+
+    public AppUserService(/*AppUserRepository repository*/) {
+        // this.repository = repository;
+    }
+
+    public boolean isUserBlacklisted(String userId) {
+        return false; //TODO:
+        // return repository.findById(userId)
+        //         .map(AppUser::isBlacklisted)
+        //         .orElse(false);
+    }
+
+    public boolean isUserActive(String userId) {
+        return true; //TODO:
+        // return repository.findById(userId)
+        //         .map(AppUser::isActive)
+        //         .orElse(false);
+    }
+
+    public Optional<String> getUserStatus(String userId) {
+        return Optional.ofNullable("TurboVIP"); //TODO:
+        // return repository.findById(userId)
+        //         .map(AppUser::getStatus);
+    }
+
+    // public Optional<AppUser> getUserById(String userId) {
+    //     return Optional.ofNullable(new AppUser("UserX", "VIP", false, true)); //TODO:
+    // }
+
+    // public boolean isVipUser(String userId) {
+    //     return true; //TODO:
+    //     // return repository.findById(userId)
+    //     //         .map(user -> "VIP".equalsIgnoreCase(user.getStatus()))
+    //     //         .orElse(false);
+    // }
+
+    // public boolean isAuthorized(String userId) {
+    //     return true; //TODO:
+    //     // return repository.findById(userId)
+    //     //         .map(user -> user.isActive() && !user.isBlacklisted())
+    //     //         .orElse(false);
+    // }
+}
